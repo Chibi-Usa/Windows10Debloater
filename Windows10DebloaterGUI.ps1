532935d1b7e799c1c726db8b28479d141c092d15
Aris A <82681339+Chibi-Usa@users.noreply.github.com>
2023-07-03 01:14:33 -0700

    Update Windows10DebloaterGUI.ps1
    i removed things that i want on my computer, generally just xbox identity providers, photos, the web media extensions, and several other things
    
    
    *if i accidentally create a pull request please deny it*

--- Windows10DebloaterGUI.ps1
+++ Windows10DebloaterGUI.ps1
@@ -53,25 +53,12 @@ $global:Bloatware = @(
     "Microsoft.Office.Sway"
     "Microsoft.OneConnect"
     "Microsoft.People"
-    "Microsoft.Print3D"
-    "Microsoft.RemoteDesktop"                           # Issue 120
+    "Microsoft.Print3D"                 # Issue 120
     "Microsoft.SkypeApp"
-    "Microsoft.StorePurchaseApp"
     "Microsoft.Office.Todo.List"                        # Issue 77
-    "Microsoft.Whiteboard"                              # Issue 77
-    "Microsoft.WindowsAlarms"
     "microsoft.windowscommunicationsapps"
-    "Microsoft.WindowsFeedbackHub"
     "Microsoft.WindowsMaps"
     "Microsoft.WindowsSoundRecorder"
-    "Microsoft.Xbox.TCUI"
-    "Microsoft.XboxApp"
-    "Microsoft.XboxGameOverlay"
-    "Microsoft.XboxGamingOverlay"
-    "Microsoft.XboxIdentityProvider"
-    "Microsoft.XboxSpeechToTextOverlay"
-    "Microsoft.ZuneMusic"
-    "Microsoft.ZuneVideo"
 
     #Sponsored Windows 10 AppX Apps
     #Add sponsored/featured apps to remove in the "*AppName*" format
@@ -87,7 +74,6 @@ $global:Bloatware = @(
     "Twitter"
     "Facebook"
     "Spotify"                                           # Issue 123
-    "Minecraft"
     "Royal Revolt"
     "Sway"                                              # Issue 77
     "Dolby"                                             # Issue 78
@@ -101,31 +87,13 @@ $global:Bloatware = @(
 #Valuable Windows 10 AppX apps that most people want to keep. Protected from DeBloat All.
 #Credit to /u/GavinEke for a modified version of my whitelist code
 $global:WhiteListedApps = @(
-    "Microsoft.WindowsCalculator"               # Microsoft removed legacy calculator
-    "Microsoft.WindowsStore"                    # Issue 1
-    "Microsoft.Windows.Photos"                  # Microsoft disabled/hid legacy photo viewer
     "CanonicalGroupLimited.UbuntuonWindows"     # Issue 10
-    "Microsoft.Xbox.TCUI"                       # Issue 25, 91  Many home users want to play games
-    "Microsoft.XboxApp"
-    "Microsoft.XboxGameOverlay"
-    "Microsoft.XboxGamingOverlay"               # Issue 25, 91  Many home users want to play games
-    "Microsoft.XboxIdentityProvider"            # Issue 25, 91  Many home users want to play games
-    "Microsoft.XboxSpeechToTextOverlay"
     "Microsoft.MicrosoftStickyNotes"            # Issue 33  New functionality.
     "Microsoft.MSPaint"                         # Issue 32  This is Paint3D, legacy paint still exists in Windows 10
     "Microsoft.WindowsCamera"                   # Issue 65  New functionality.
-    "\.NET"
-    "Microsoft.HEIFImageExtension"              # Issue 68
-    "Microsoft.ScreenSketch"                    # Issue 55: Looks like Microsoft will be axing snipping tool and using Snip & Sketch going forward
-    "Microsoft.StorePurchaseApp"                # Issue 68
-    "Microsoft.VP9VideoExtensions"              # Issue 68
-    "Microsoft.WebMediaExtensions"              # Issue 68
-    "Microsoft.WebpImageExtension"              # Issue 68
-    "Microsoft.DesktopAppInstaller"             # Issue 68
     "WindSynthBerry"                            # Issue 68
     "MIDIBerry"                                 # Issue 68
     "Slack"                                     # Issue 83
-    "*Nvidia*"                                  # Issue 198
     "Microsoft.MixedReality.Portal"             # Issue 195
 )
 
